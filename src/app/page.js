5c90db45c057f7378c617453f6c38c75da84b6d7
LemonPepperSeasoning <takahiro.swe@gmail.com>
2024-05-24 04:56:58 +0900

    fixed links

--- src/app/page.js
+++ src/app/page.js
@@ -26,7 +26,7 @@ export default function Home() {
 
           <NavBar />
 
-          <div className="font-medium text-2xl pb-3">
+          <div className="pb-3 text-2xl font-medium">
             Hi, I'm Takahiro 👋
           </div>
 
@@ -37,13 +37,9 @@ export default function Home() {
             <p>
               I'm interested in Backend, Robotics & AI.
             </p>
-
-            <p className='pt-10'>
-              more content to come ...
-            </p>
           </div>
 
-          <ul className="flex flex-wrap items-center justify-center text-gray-900 dark:text-white space-x-4 mb-10">
+          <ul className="flex flex-wrap items-center justify-center mb-10 space-x-4 text-gray-900 dark:text-white">
             <li>
               <a href="https://github.com/LemonPepperSeasoning" target="_blank" rel="noopener" aria-label="brands/github">
                 <svg style={{ height: "1.5rem" }} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 28" className="hover:scale-125">
@@ -53,7 +49,7 @@ export default function Home() {
             </li>
 
             <li>
-              <a href="mailto:takahiro.swe@example.com" aria-label="at-symbol" data-toggle="tooltip" data-placement="top" title="E-mail Me">
+              <a href="mailto:takahiro.swe@gmail.com" aria-label="at-symbol" data-toggle="tooltip" data-placement="top" title="E-mail Me">
                 <svg style={{ height: "1.25rem" }} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="hover:scale-125">
                   <path fill="none" stroke="black" stroke-linecap="round" stroke-width="1.5" d="M16.5 12a4.5 4.5.0 11-9 0 4.5 4.5.0 019 0zm0 0c0 1.657 1.007 3 2.25 3S21 13.657 21 12a9 9 0 10-2.636 6.364M16.5 12V8.25" />
                 </svg>
@@ -67,7 +63,7 @@ export default function Home() {
               </a>
             </li>
             <li>
-              <a href="https://www.instagram.com/" target="_blank" rel="noopener" aria-label="brands/instagram">
+              <a href="https://www.instagram.com/_takahiro___/" target="_blank" rel="noopener" aria-label="brands/instagram">
                 <svg style={{ height: "1.25rem" }} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 15 15" className="hover:scale-125">
                   <path fill="black" fill-rule="evenodd" d="M12.91 12.909c.326-.327.582-.72.749-1.151.16-.414.27-.886.302-1.578.032-.693.04-.915.04-2.68.0-1.765-.008-1.987-.04-2.68-.032-.692-.142-1.164-.302-1.578a3.185 3.185.0 00-.75-1.151 3.187 3.187.0 00-1.151-.75c-.414-.16-.886-.27-1.578-.302C9.487 1.007 9.265 1 7.5 1c-1.765.0-1.987.007-2.68.04-.692.03-1.164.14-1.578.301a3.2 3.2.0 00-1.151.75 3.2 3.2.0 00-.75 1.151c-.16.414-.27.886-.302 1.578C1.007 5.513 1 5.735 1 7.5s.007 1.987.04 2.68c.03.692.14 1.164.301 1.578.164.434.42.826.75 1.151.325.33.718.586 1.151.75.414.16.886.27 1.578.302.693.031.915.039 2.68.039s1.987-.008 2.68-.04c.692-.03 1.164-.14 1.578-.301a3.323 3.323.0 001.151-.75zM2 6.735v1.53c-.002.821-.002 1.034.02 1.5.026.586.058 1.016.156 1.34.094.312.199.63.543 1.012.344.383.675.556 1.097.684.423.127.954.154 1.415.175.522.024.73.024 1.826.024H8.24c.842.001 1.054.002 1.526-.02.585-.027 1.015-.059 1.34-.156.311-.094.629-.2 1.011-.543.383-.344.556-.676.684-1.098.127-.422.155-.953.176-1.414C13 9.247 13 9.04 13 7.947v-.89c0-1.096.0-1.303-.023-1.826-.021-.461-.049-.992-.176-1.414-.127-.423-.3-.754-.684-1.098-.383-.344-.7-.449-1.011-.543-.325-.097-.755-.13-1.34-.156A27.29 27.29.0 008.24 2H7.057c-1.096.0-1.304.0-1.826.023-.461.021-.992.049-1.415.176-.422.128-.753.301-1.097.684s-.45.7-.543 1.012c-.098.324-.13.754-.156 1.34-.022.466-.022.679-.02 1.5zM7.5 5.25a2.25 2.25.0 100 4.5 2.25 2.25.0 000-4.5zM4.25 7.5a3.25 3.25.0 116.5.0 3.25 3.25.0 01-6.5.0zm6.72-2.72a.75.75.0 100-1.5.75.75.0 000 1.5z" clip-rule="evenodd" />
                 </svg>
@@ -90,37 +86,37 @@ export default function Home() {
           </ul>
 
 
-          <div className="columns-2 sm:columns-3 gap-4">
+          <div className="gap-4 columns-2 sm:columns-3">
             <img
               src="/static/img/IMG_1592.PNG"
               alt="Picture of the author"
-              className="object-cover rounded-lg mb-2"
+              className="object-cover mb-2 rounded-lg"
             />
 
 
             <img
               src="/static/img/IMG_5514.PNG"
               alt="Picture of the author"
-              className="object-cover w-60 rounded-lg mb-2"
+              className="object-cover mb-2 rounded-lg w-60"
             />
 
             <img
               src="/static/img/IMG_6320.PNG"
               alt="Picture of the author"
-              className="object-cover w-60 rounded-lg mb-2"
+              className="object-cover mb-2 rounded-lg w-60"
             />
 
 
             <img
               src="/static/img/IMG_9426.PNG"
               alt="Picture of the author"
-              className="object-cover w-60 rounded-lg mb-2"
+              className="object-cover mb-2 rounded-lg w-60"
             />
 
             <img
               src="/static/img/IMG_6599.PNG"
               alt="Picture of the author"
-              className="object-cover w-60 rounded-lg mb-2"
+              className="object-cover mb-2 rounded-lg w-60"
             />
           </div>
         </div >
